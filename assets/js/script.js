4cc6dbe51697d66204c1b3c89998ab40451760a0
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-05-06 20:39:35 -0400

    identify if question is righ or wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,8 @@
 var score = 0;
-var currentQuestion = 0;
+var currentQuestion = -1;
 var timeLeft = 75;
 var startQuizBtn = document.querySelector('#start-quiz');
 var parent = document.querySelector('.body');
-var questionContainer = document.querySelector('.main-screen');
-var screen = document.querySelector('.body');
 var timer = document.querySelector('.timer');
 var goBackBtn = document.getElementById('go-back');
 
@@ -21,13 +19,14 @@ var startTimer = function countdown() {
         else {
             timer.textContent = 0;
             clearInterval(timeInterval);
+            endgame();
         }
     }, 1000);
 }
 
 var cleanScreen = function() {
 
-    questionContainer.parentNode.removeChild(questionContainer);
+    parent.removeChild(document.querySelector('.main-screen'));
 
     createQuestionEl();
 }
@@ -89,11 +88,14 @@ var questions = [
 //create question element
 var createQuestionEl = function() {
     
+    //change which question we are on
+    currentQuestion++;
+
     //create question element
-    var questionEl = document.createElement('section');
+    questionEl = document.createElement('section');
     questionEl.className = 'main-screen';
     questionEl.setAttribute('question-id', currentQuestion);
-    screen.appendChild(questionEl);
+    parent.appendChild(questionEl);
 
     //create question
     var questionContent = document.createElement('h1');
@@ -104,52 +106,56 @@ var createQuestionEl = function() {
     for (i = 0; i < questions[currentQuestion].choices.length; i++) {
         var questionAnswer = document.createElement('button');
         questionAnswer.textContent = questions[currentQuestion].choices[i];
-        questionAnswer.className = 'btn ' + 'q' + i;
+        questionAnswer.className = 'btn';
+        questionAnswer.id = i
         questionEl.appendChild(questionAnswer);
-        questionAnswer.addEventListener('click', function() {checkAnswer(i)});
+        questionAnswer.addEventListener('click', checkAnswer);
     }
-
-    if(currentQuestion > questions.length || timeLeft < 1) {
-        endgame();
-    }
-
-    //change which question we are on
-    currentQuestion++;
+    
 }
 
 
 //check answer submitted
-var checkAnswer = function(id) {
+var checkAnswer = function(e) {
 
+    // console.log(e.target.id);
+    // console.log(questions[currentQuestion].answer);
     //check if correct button was clicked and add a point
-    if(id === questions[currentQuestion].answer) {
+    if( e.target.id == questions[currentQuestion].answer) {
         var showResult = document.createElement('h2');
         showResult.textContent = 'Correct';
-        showResult.className = 'show-result';
-        screen.appendChild(showResult);
+        showResult.className = '.show-result';
+        questionEl.appendChild(showResult);
         
         //add 1 to score
         score++;
+        
+        setTimeout(cleanScreen, 1000);
+
     }
 
     //if wrong anser was clicked subtract 10 seconds
     else {
         var showResult = document.createElement('h2');
         showResult.textContent = 'Incorrect';
-        showResult.className = 'show-result';
-        screen.appendChild(showResult);
+        showResult.className = '.show-result';
+        questionEl.appendChild(showResult);
 
         // subtract 10 seccounds from timer
-        timeLeft - 10;
+        timeLeft -= 10;
+        
+        setTimeout(cleanScreen, 1000);
     }
 
-    cleanScreen();
+    if (currentQuestion >= questions.length - 1) {
+        endgame();
+    }
 }
 
 
 //game ends with answering all questions or timer running out
 var endgame = function() {
-
+    console.log('blue whales');
     //show score
 
         // input initials for scoreboard
@@ -159,25 +165,11 @@ var endgame = function() {
 }
 
 var saveScore = function() {
-    
+    console.log('yellow whale');
     //store the scoreboard in local storage if they play again
     localStorage.setItem('highscore', score);
 }
 
-var goBack = function() {
-    score = 0;
-    timeLeft = 75;
-    currentQuestion = 0;
-
-    console.log('blue whales!');
-}
 
 
 startQuizBtn.addEventListener('click', startQuiz);
-
-
-
-
-//goBackBtn.addEventListener('click', goBack);
-
-
